9f804e36948ea34cab75c710034f27d2f61af46a
김식혜 <139305586+GimSikhye@users.noreply.github.com>
2025-03-20 11:21:57 +0900

    커피머신 터치했을 경우, 팝업창 띄우기
    일정거리 이하이면 팝업창을 띄우고,
    일정거리 이상이면 멀다는 주의문구를 띄움.

---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,52 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    public static UIManager Instance;
+    [SerializeField] private GameObject[] panels;
+    //[SerializeField] private GameObject captionText;
+    [SerializeField] private TextMeshProUGUI captionTmp;
+
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        foreach(var item  in panels)
+        {
+            item.SetActive(false);  
+        }
+        captionTmp.enabled = false;
+    }
+
+    public void ShowPopup()
+    {
+        panels[0].SetActive(true);
+    }
+
+    public void ShowCapitonText()
+    {
+        Vector3 playerScreenPos = Camera.main.WorldToScreenPoint(PlayerCtrl.Instance.transform.position);
+
+        captionTmp.rectTransform.position = playerScreenPos;
+        captionTmp.enabled = true;
+        captionTmp.text = "거리가 너무 멀어요!";
+    }
+
+
+
+}
---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    [SerializeField] private Image touch_feedback;
+    [SerializeField] Transform coffeeMachine;
+
+    [SerializeField] private float interactionRange;
+
+    public static PlayerCtrl Instance;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void Update()
+    {
+        OnTouch();
+    }
+
+    private void OnTouch()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+            {
+                ShowTouchFeedback(touch.position);
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                Debug.Log("손가락을 뗀 것이 감지됨!!");
+
+                // 이동할 수 있는 곳이면
+                //OnMove(touch);
+
+                
+                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, Camera.main.nearClipPlane)); //터치한 지점
+                Collider2D hitCollider = Physics2D.OverlapPoint(worldPosition);
+
+                if (hitCollider != null && hitCollider.transform == coffeeMachine)
+                {
+                    // 거리도 확인해서 가까울 경우만 팝업 표시
+                    if (Vector3.Distance(transform.position, coffeeMachine.position) < interactionRange)
+                    {
+                        UIManager.Instance.ShowPopup(); // UIManager의 팝업 표시 함수 호출
+                    }
+                    else
+                    {
+                        Debug.Log("거리가 너무 멀어요!!");
+                        UIManager.Instance.ShowCapitonText();
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnMove(Touch touch)
+    {
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, Camera.main.nearClipPlane));
+        worldPosition.z = 0;
+        transform.position = worldPosition;
+
+        touch_feedback.enabled = false;
+    }
+
+    private void ShowTouchFeedback(Vector2 screenPosition)
+    {
+        // UI 요소이므로 localPosition을 사용하여 캔버스 내에서 좌표 설정
+        touch_feedback.rectTransform.position = screenPosition;
+        touch_feedback.enabled = true;
+    }
+
+
+
+    
+}
