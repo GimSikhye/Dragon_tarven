17c3cf0a83f3cec4f7923bcf3a50960a3c4a98b7
김식혜 <139305586+GimSikhye@users.noreply.github.com>
2025-03-21 21:30:47 +0900

    드래그로 화면 이동하기
    손가락이 움직이는 방향으로 바로 이동하면 카메라가 손가락을 따라가 버려서 조작이 어색해 반대 방향으로 해주었습니다.

--- Assets/02Scripts/CameraController.cs
+++ Assets/02Scripts/CameraController.cs
@@ -3,28 +3,44 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public float dragSpeed = 0.1f; // 드래그 속도 조절
-    public Vector2 minLimit; // 카메라 이동 최소 좌표(x,y)
-    public Vector2 maxLimit; // 카메라 이동 최대 좌표(x,y)
+    public Vector2 minBounds; // 최소 좌표(왼쪽 아래)
+    public Vector2 maxBounds; // 최대 좌표(오른쪽 위)
 
-    private Vector3 dragOrigin; // 드래그 시작점
-
-
-    void Start()
-    {
-        
-    }
+    private Vector3 lastTouchPosition;
 
     void Update()
     {
-        HandleTouchInput();
+        HandleCameraDrag();
     }
 
-    private void HandleTouchInput()
+    private void HandleCameraDrag()
     {
         if(Input.touchCount == 1)
         {
             Touch touch = Input.GetTouch(0);
 
+            if(touch.phase == TouchPhase.Began)
+            {
+                lastTouchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            }
+            else if(touch.phase == TouchPhase.Moved)
+            {
+                Vector3 currentPosition = Camera.main.ScreenToWorldPoint(touch.position);
+                Vector3 direction = currentPosition - lastTouchPosition; // 이동방향 계산
+                MoveCamera(-direction);
+                lastTouchPosition = currentPosition;
+            }
         }
+    }
+
+    private void MoveCamera(Vector3 moveDirection)
+    {
+        Vector3 newPosition = transform.position + moveDirection * dragSpeed; // 새로운 위치 계산
+
+        // 카메라 위치를 제한된 영역 안으로 조정
+        newPosition.x = Mathf.Clamp(newPosition.x, minBounds.x, maxBounds.x);
+        newPosition.y = Mathf.Clamp(newPosition.y, minBounds.y, maxBounds.y);
+
+        transform.position = newPosition;
     }
 }
