93b96e4575559627c58d7a6a3db0def4b6d5638a
김식혜 <139305586+GimSikhye@users.noreply.github.com>
2025-03-23 10:59:58 +0900

    메뉴 생성
    (+ sprite 이미지 추가 예정)
    메뉴에 필요한 데이터 추가

---
+++ Assets/08SO/CoffeeData.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CoffeeData", menuName = "SO/CoffeeData")]
+public class CoffeeData : ScriptableObject
+{
+    [SerializeField]
+    private string coffeeName;
+    public string CoffeName { get { return coffeeName; } }
+    [SerializeField]
+    private int price;
+    public int Price { get { return price; } }
+    [SerializeField]
+    private int mugQty;
+    public int MugQty { get { return mugQty; } }
+    [SerializeField]
+    private int beanUse;
+    public int BeanUse { get { return beanUse; } }
+}
