e0430bd178bacd5613dfa9891707eb2af1a1ce75
김식혜 <139305586+GimSikhye@users.noreply.github.com>
2025-03-24 12:04:58 +0900

    mlagent 경로 수정
    그래프 노드 크기 수정

--- Assets/02Scripts/Customer/Customer.cs
+++ Assets/02Scripts/Customer/Customer.cs
@@ -5,6 +5,9 @@ public class Customer : MonoBehaviour
 {
     private Transform target; // 이동할 목표 지점
     private AIPath aiPath; // A* 경로 탐색을 위한 AIPath 컴포넌트
+    GameObject pathParent;
+    Transform[] pathPoints;
+    private int currentIndex = 0;
 
     void Start()
     {
@@ -20,10 +23,10 @@ public class Customer : MonoBehaviour
 
     void SetNextDestination()
     {
-        GameObject pathParent = GameObject.Find("PathPoints");
+        pathParent = GameObject.Find("PathPoints");
         if (pathParent != null)
         {
-            Transform[] pathPoints = new Transform[pathParent.transform.childCount];
+            pathPoints = new Transform[pathParent.transform.childCount];
             for (int i = 0; i < pathPoints.Length; i++)
             {
                 pathPoints[i] = pathParent.transform.GetChild(i);
@@ -37,12 +40,12 @@ public class Customer : MonoBehaviour
         }
     }
 
-    void Update() // ??
+    void Update() 
     {
         // 목표 지점에 도착하면 다음 지점으로 변경
         if (aiPath.reachedDestination && target != null)
         {
-            target = target.GetChild(0); // 다음 목표 지점
+            target = pathPoints[++currentIndex]; // 다음 목표 지점
             aiPath.destination = target.position;
         }
     }
