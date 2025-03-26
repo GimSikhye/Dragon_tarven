6151eec09f1121d75399672eaff95c44f34bbaeb
김식혜 <139305586+GimSikhye@users.noreply.github.com>
2025-03-26 22:33:26 +0900

    손님 주문 sprite 변경
    주문할 메뉴를 말풍선에 띄움

--- Assets/02Scripts/Customer/Customer.cs
+++ Assets/02Scripts/Customer/Customer.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
-using Pathfinding; // A* Pathfinding 네임스페이스 추가
+using Pathfinding;
+using System.Collections.Generic;
+using NUnit.Framework; // A* Pathfinding 네임스페이스 추가
 
 public class Customer : MonoBehaviour
 {
-    private Transform target; // 이동할 목표 지점
     private AIPath aiPath; // A* 경로 탐색을 위한 AIPath 컴포넌트
+    private CustomerPool custerPool;
+    private Transform target; // 이동할 목표 지점
     GameObject pathParent;
     Transform[] pathPoints;
     private int currentIndex = 0;
-    private CustomerPool custerPool;
+
+    [Header("주문 관련")]
+    [SerializeField] private GameObject speechBalloon;
+    [SerializeField] private SpriteRenderer orderMenuSprite;
+    private bool isOrdering = false; 
 
     void Start()
     {
@@ -41,6 +48,12 @@ public class Customer : MonoBehaviour
     {
         if (aiPath.reachedDestination && target != null)
         {
+            if(currentIndex == 1 && !isOrdering)
+            {
+                StartOrdering();
+                return;
+            }
+
             currentIndex++;
 
             if (currentIndex >= pathPoints.Length)
@@ -53,5 +66,38 @@ public class Customer : MonoBehaviour
             target = pathPoints[currentIndex];
             aiPath.destination = target.position;
         }
+    }
+
+    // 주문을 시작하는 함수
+    void StartOrdering()
+    {
+        isOrdering = true;
+        aiPath.canMove = false; // 이동 중지
+        speechBalloon.SetActive(true);
+
+        // 모든 커피머신에서 랜덤한 커피 선택
+        CoffeeMachine[] coffeeMachines = FindObjectsOfType<CoffeeMachine>();
+        if(coffeeMachines.Length > 0 )
+        {
+            List<CoffeeData> availableCoffees = new List<CoffeeData>();
+            foreach(var machine in coffeeMachines)
+            {
+                if(machine.CurrentCoffee != null)
+                {
+                    availableCoffees.Add(machine.CurrentCoffee);
+                }
+            }
+
+            if(availableCoffees.Count > 0)
+            {
+                CoffeeData randomCoffee = availableCoffees[Random.Range(0, availableCoffees.Count)];
+                orderMenuSprite.sprite = randomCoffee.MenuIcon;
+            }
+            else
+            {
+                Debug.Log("주문 가능한 커피가 없습니다!");
+            }
+        }
+
     }
 }
