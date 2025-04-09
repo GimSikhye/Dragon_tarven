bdeb16e7aba9a879348888962b0afe53d88cc9de
김식혜 <139305586+GimSikhye@users.noreply.github.com>
2025-04-09 10:26:31 +0900

    퀘스트 조건 한글로 표시

--- Assets/02Scripts/Quest/QuestUI.cs
+++ Assets/02Scripts/Quest/QuestUI.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -37,16 +38,54 @@ public class QuestUI : MonoBehaviour
         foreach (Transform child in conditionParent) // 이전에 표시돼 있던 조건 UI 항목들을 싹 지움
             Destroy(child.gameObject);
 
-        foreach (var cond in quest.conditions) // 조건 하나씩 UI로 생성 및 표시
+        foreach (var cond in quest.conditions)
         {
             var go = Instantiate(conditionTextPrefab, conditionParent);
-            var text = go.GetComponentInChildren<TextMeshProUGUI>();
+            var text = go.GetComponent<TextMeshProUGUI>();
+
             string state = cond.currentAmount >= cond.requiredAmount ? "(완료)" : "";
-            text.text = $"({cond.type}) {cond.targetItemId} {cond.currentAmount}/{cond.requiredAmount} {state}"; // 예: (SellItem) americano 2/3, (PlaceFurniture) table_01 1/1 (완료)
+
+            // 타입별 한글 문장 구성
+            string conditionText = "";
+
+            string displayName = GetDisplayName(cond.targetItemId); // 한글 이름 변환 함수
+
+            switch (cond.type)
+            {
+                case QuestConditionType.SellItem:
+                    conditionText = $"{displayName} {cond.requiredAmount}잔 판매 {cond.currentAmount}/{cond.requiredAmount} {state}";
+                    break;
+                case QuestConditionType.PlaceFurniture:
+                    conditionText = $"{displayName} {cond.requiredAmount}개 배치 {cond.currentAmount}/{cond.requiredAmount} {state}";
+                    break;
+                case QuestConditionType.UpgradeInterior:
+                    conditionText = $"{displayName} 업그레이드 {cond.currentAmount}/{cond.requiredAmount} {state}";
+                    break;
+            }
+
+            text.text = conditionText;
         }
+
 
         questPanel.SetActive(true);
     }
+
+    private string GetDisplayName(string id)
+    {
+        // 커피 & 가구 아이디에 따른 한글 이름 매핑
+        Dictionary<string, string> nameMap = new Dictionary<string, string>()
+    {
+        { "americano", "아메리카노" },
+        { "latte", "라떼" },
+        { "espresso", "에스프레소" },
+        { "chair_01", "의자1" },
+        { "table_01", "테이블1" },
+        { "counter_01", "카운터1" },
+    };
+
+        return nameMap.TryGetValue(id, out var name) ? name : id;
+    }
+
 
     public void ShowQuestComplete(QuestData quest)
     {
