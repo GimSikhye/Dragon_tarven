73125593fbf882d2a6aebbebb15b003357d964be
김식혜 <139305586+GimSikhye@users.noreply.github.com>
2025-04-25 18:43:09 +0900

    아이템 분류

---
+++ Assets/02Scripts/Data/ItemTypeData.cs
@@ -0,0 +1,40 @@
+public enum ItemCategory
+{
+    Kitchen, // 주방
+    Interior, // 인테리어
+    Exterior // 익스테리어
+}
+
+public enum KitchenType
+{
+    RoastingMachine, // 로스팅머신
+    CoffeeMachine, // 커피머신
+    Workbench, // 작업대
+    CookingMachine, // 쿠킹머신
+    Showcase, // 쇼케이스
+    Counter, // 계산대
+    Mixer // 믹서기
+}
+
+public enum InteriorType
+{
+    Table, // 테이블
+    Chair, // 의자
+    Partition, // 파티션
+    Decoration, // 장식품 
+    BeanContainer, // 원두통
+    WallDecoration, // 벽장식
+    Tile, // 타일
+    Wallpaper // 벽지
+}
+
+public enum ExteriorType
+{
+    SecondFloorOnly, // 2층전용
+    OutdoorDecoration, // 야외장식품
+    WallExteriorDecoration, // 건물 외벽 장식
+    Railing2F, // 2층 난간
+    Stair2F, // 2층 계단
+    WallExterior, // 건물 외벽
+    Entrance // 입구
+}
