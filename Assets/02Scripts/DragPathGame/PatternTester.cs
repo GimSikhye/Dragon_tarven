1682bdd4379164db90c10c0fedb0c23e587a1ea3
김식혜 <139305586+GimSikhye@users.noreply.github.com>
2025-05-07 15:43:15 +0900

    그려지는 노드 크기 수정

--- Assets/02Scripts/DragPathGame/PatternTester.cs
+++ Assets/02Scripts/DragPathGame/PatternTester.cs
@@ -5,13 +5,13 @@ public class PatternTester : MonoBehaviour
     public StainPatternGenerator generator;
     void Start()
     {
-        Vector3[] starShape = new Vector3[]
+        Vector3[] starShape = new Vector3[] // 현재 위치가 어떻게 그려지는지 확인해야함. (원리)
         {
-            new Vector3(-100, 0, 0),
-            new Vector3(0, 150, 0),
-            new Vector3(100, 0, 0),
-            new Vector3(-100, 75, 0),
-            new Vector3(100, 75, 0),
+            new Vector3(-30, 0, 0), // 
+            new Vector3(0, 45, 0), // x : 100 y : 150
+            new Vector3(30, 0, 0), // // x : 200
+            new Vector3(-30, 30, 0), // y : 75
+            new Vector3(30, 30, 0),
 
         };
 
--- Assets/02Scripts/DragPathGame/StainPatternGenerator.cs
+++ Assets/02Scripts/DragPathGame/StainPatternGenerator.cs
@@ -6,18 +6,27 @@ public class StainPatternGenerator : MonoBehaviour
     public Transform nodeParent;
     public GameObject nodePrefab;
     public List<Transform> currentPath = new();
+    public LineRenderer pathLineRenderer;
 
     public void GeneratePattern(Vector3[] points)
     {
         foreach (Transform child in nodeParent) Destroy(child.gameObject);
+
         currentPath.Clear();
 
-        foreach(Vector3 pos in points)
+        // 포인트 수에 따라 LineRenderer 초기화
+        pathLineRenderer.positionCount = points.Length;
+
+        for (int i = 0; i < points.Length; i++)
         {
-            GameObject node = Instantiate(nodePrefab, nodeParent);
+            Vector3 pos = points[i];
+            GameObject node = Instantiate(nodePrefab, nodeParent); // nodeParent 자식으로 생성
             node.transform.localPosition = pos;
             currentPath.Add(node.transform);
+
+            // 연결선도 같이 그려줌
+            Vector3 worldPos = node.transform.position;
+            pathLineRenderer.SetPosition(i, worldPos);
         }
     }
- 
 }
