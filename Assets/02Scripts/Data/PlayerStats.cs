171a3b360ae4b02895cabdc9acfe52bad0bee654
Park Jinhee <pjhee07@naver.com>
2025-09-08 20:21:53 +0900

    다이얼로그 씬 연결

--- Assets/02Scripts/Data/PlayerStats.cs
+++ Assets/02Scripts/Data/PlayerStats.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerStats", menuName = "SO/PlayerStats")]
 public class PlayerStats : ScriptableObject
 {
-    public int coffeeBean = 0;
-    public int coin = 0;
-    public int gem = 0;
+    public int coffeeBean = 1000;
+    public int coin = 100;
+    public int gem = 10;
 }
--- Assets/02Scripts/UI/MainMenuUI.cs
+++ Assets/02Scripts/UI/MainMenuUI.cs
@@ -6,10 +6,20 @@ public class MainMenuUI : MonoBehaviour
 {
     [SerializeField] private Button startButton;
     [SerializeField] private Button quitButton;
+    [SerializeField] private PlayerStats playerStats;
 
     private void Awake()
     {
-        startButton.onClick.AddListener(() => SceneManager.LoadScene("GameScene"));
+        startButton.onClick.AddListener(() =>
+        {
+            // 재화 초기화
+            playerStats.coffeeBean = 1000;
+            playerStats.coin = 100;
+            playerStats.gem = 10;
+
+            SceneManager.LoadScene("DialogueScene");
+        });
+        //startButton.onClick.AddListener(() => SceneManager.LoadScene("DialogueScene"));
         quitButton.onClick.AddListener(() => Application.Quit());
     }
 
