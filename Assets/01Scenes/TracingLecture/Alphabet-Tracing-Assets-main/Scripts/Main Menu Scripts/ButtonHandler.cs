ae6ed855daaf2b4c5e9f0408bc7891c2c4760f3a
김식혜 <139305586+GimSikhye@users.noreply.github.com>
2025-09-09 22:59:59 +0900

    오류 수정, 에셋 정리

--- Assets/01Scenes/TracingLecture/Alphabet-Tracing-Assets-main/Scripts/Main Menu Scripts/ButtonHandler.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class ButtonHandler : MonoBehaviour
-{
-    public void ActivePanel(string _panelName)
-    {
-        MainMenuHandler.Instance.panelName = _panelName;
-    }
-}
