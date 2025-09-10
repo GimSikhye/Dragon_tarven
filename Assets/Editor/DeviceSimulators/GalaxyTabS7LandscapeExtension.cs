dabdbef464e70b2ce8d1c44c956383a516c07191
Park Jinhee <pjhee07@naver.com>
2025-09-10 22:06:27 +0900

    음료만들기 수정

--- Assets/Editor/DeviceSimulators/GalaxyTabS7LandscapeExtension.cs
+++ Assets/Editor/DeviceSimulators/GalaxyTabS7LandscapeExtension.cs
@@ -1,18 +1,17 @@
 //#if UNITY_EDITOR
 //using UnityEditor;
-//using UnityEngine;
-//using UnityEngine.DeviceInfo;
+//using UnityEditor.DeviceSimulation;
 
 //public class GalaxyTabS7LandscapeExtension : IDeviceSimulatorExtension
 //{
 //    public string extensionName => "Galaxy Tab S7+ Landscape Auto-Rotate";
 
-//    public void OnCreate(ISimulator simulator)
+//    public void OnCreate(IDeviceSimulator simulator)
 //    {
 //        if (simulator.deviceInfo != null &&
 //            simulator.deviceInfo.friendlyName == "Galaxy Tab S7+")
 //        {
-//            simulator.rotation = DeviceRotation.LandscapeLeft;
+//            simulator.rotation = ScreenOrientation.LandscapeLeft;
 //        }
 //    }
 
